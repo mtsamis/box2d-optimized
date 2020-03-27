ea2ca5349a67c5081c295b9f6d1eabf1848d13e6
mtsamis <tsamismanolis@gmail.com>
2020-03-27 15:33:00 +0200

    Modify/improve functions in b2Collision

--- include/box2d/b2_collision.h
+++ include/box2d/b2_collision.h
@@ -163,62 +163,53 @@ struct b2RayCastOutput
 };
 
 /// An axis aligned bounding box.
-struct b2AABB
-{
+struct b2AABB {
 	/// Verify that the bounds are sorted.
 	bool IsValid() const;
 
 	/// Get the center of the AABB.
-	b2Vec2 GetCenter() const
-	{
+	b2Vec2 GetCenter() const {
 		return 0.5f * (lowerBound + upperBound);
 	}
 
 	/// Get the extents of the AABB (half-widths).
-	b2Vec2 GetExtents() const
-	{
+	b2Vec2 GetExtents() const {
 		return 0.5f * (upperBound - lowerBound);
 	}
 
 	/// Get the perimeter length
-	float GetPerimeter() const
-	{
+	float GetPerimeter() const {
 		float wx = upperBound.x - lowerBound.x;
 		float wy = upperBound.y - lowerBound.y;
 		return 2.0f * (wx + wy);
 	}
 	
 	/// Get the perimeter length
-	float GetArea() const
-	{
+	float GetArea() const {
 		float wx = upperBound.x - lowerBound.x;
 		float wy = upperBound.y - lowerBound.y;
 		return wx * wy;
 	}
 
 	/// Combine an AABB into this one.
-	void Combine(const b2AABB& aabb)
-	{
+	void Combine(const b2AABB& aabb) {
 		lowerBound = b2Min(lowerBound, aabb.lowerBound);
 		upperBound = b2Max(upperBound, aabb.upperBound);
 	}
 
 	/// Combine two AABBs into this one.
-	void Combine(const b2AABB& aabb1, const b2AABB& aabb2)
-	{
+	void Combine(const b2AABB& aabb1, const b2AABB& aabb2) {
 		lowerBound = b2Min(aabb1.lowerBound, aabb2.lowerBound);
 		upperBound = b2Max(aabb1.upperBound, aabb2.upperBound);
 	}
 
 	/// Does this aabb contain the provided AABB.
-	bool Contains(const b2AABB& aabb) const
-	{
-		bool result = true;
-		result = result && lowerBound.x <= aabb.lowerBound.x;
-		result = result && lowerBound.y <= aabb.lowerBound.y;
-		result = result && aabb.upperBound.x <= upperBound.x;
-		result = result && aabb.upperBound.y <= upperBound.y;
-		return result;
+	bool Contains(const b2AABB& aabb) const {
+		// use & instead of && to prevent short-circuit
+		return (lowerBound.x <= aabb.lowerBound.x)
+				 & (lowerBound.y <= aabb.lowerBound.y)
+				 & (aabb.upperBound.x <= upperBound.x)
+				 & (aabb.upperBound.y <= upperBound.y);
 	}
 
 	bool RayCast(b2RayCastOutput* output, const b2RayCastInput& input) const;
@@ -263,27 +254,19 @@ bool b2TestOverlap(	const b2Shape* shapeA,
 
 // ---------------- Inline Functions ------------------------------------------
 
-inline bool b2AABB::IsValid() const
-{
-	b2Vec2 d = upperBound - lowerBound;
-	bool valid = d.x >= 0.0f && d.y >= 0.0f;
-	valid = valid && lowerBound.IsValid() && upperBound.IsValid();
-	return valid;
+inline bool b2AABB::IsValid() const {
+	return upperBound.x >= lowerBound.x
+			&& upperBound.y >= lowerBound.y
+			&& lowerBound.IsValid()
+			&& upperBound.IsValid();
 }
 
-inline bool b2TestOverlap(const b2AABB& a, const b2AABB& b)
-{
-	b2Vec2 d1, d2;
-	d1 = b.lowerBound - a.upperBound;
-	d2 = a.lowerBound - b.upperBound;
-
-	if (d1.x > 0.0f || d1.y > 0.0f)
-		return false;
-
-	if (d2.x > 0.0f || d2.y > 0.0f)
-		return false;
-
-	return true;
+inline bool b2TestOverlap(const b2AABB& a, const b2AABB& b) {
+	// use & instead of && to prevent short-circuit
+	return (a.upperBound.x >= b.lowerBound.x)
+			 & (a.lowerBound.x <= b.upperBound.x)
+			 & (a.upperBound.y >= b.lowerBound.y)
+			 & (a.lowerBound.y <= b.upperBound.y);
 }
 
 #endif
