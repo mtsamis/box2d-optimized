764d3bffec2b462152843de22306fa632f84377b
mtsamis <tsamismanolis@gmail.com>
2020-05-07 10:26:40 +0300

    Improve/Add benchmarks

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -21,6 +21,9 @@
 // SOFTWARE.
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
 #include <chrono>
 #include <stdio.h>
 #include <unistd.h>
@@ -28,92 +31,238 @@
 
 #include "box2d/box2d.h"
 
-void InitWorld(b2World* world) {
-  const int32 e_count = 200;
+class b2Benchmark {
+public:
+  b2Benchmark() : gravity(0.0f, -10.0f) {};
   
-  {
-    float a = 0.5f;
-	  b2BodyDef bd;
-	  bd.position.y = -a;
-	  b2Body* ground = world->CreateBody(&bd);
-	  
-	  int32 N = 200;
-	  int32 M = 10;
-	  b2Vec2 position;
-	  position.y = 0.0f;
-	  for (int32 j = 0; j < M; ++j) {
-		  position.x = -N * a;
-		  for (int32 i = 0; i < N; ++i) {
-			  b2PolygonShape shape;
-			  shape.SetAsBox(a, a, position, 0.0f);
-			  ground->CreateFixture(&shape, 0.0f);
-			  position.x += 2.0f * a;
-		  }
-		  position.y -= 2.0f * a;
-	  }
-	}
-	
-	{
-	  float a = 0.5f;
-		b2PolygonShape shape;
-		shape.SetAsBox(a, a);
-    b2FixtureDef ds;
-	  ds.shape = &shape;
-	  ds.density = 5;
-	   
-		b2Vec2 x(-7.0f, 0.75f);
-		b2Vec2 y;
-		b2Vec2 deltaX(0.5625f, 1.25f);
-		b2Vec2 deltaY(1.125f, 0.0f);
-		
-		for (int32 i = 0; i < e_count; ++i) {
-			y = x;
+  virtual void InitBenchmark() {};
+  virtual void InitWorld(b2World* world) = 0;
+  virtual void StepWorld(b2World* world) {};
 
-			for (int32 j = i; j < e_count; ++j) {
-				b2BodyDef bd;
-				bd.type = b2_dynamicBody;
-				bd.position = y;
-				bd.position.y = 190 - bd.position.y;
-
-				b2Body* body = world->CreateBody(&bd);
-				body->CreateFixture(&ds);
-				y += deltaY;
-			}
-			
-			x += deltaX;
-		}
-	}
-}
-
-int main() {
-	// Define the gravity vector.
-	b2Vec2 gravity(0.0f, -10.0f);
-
-	// Construct a world object, which will hold and simulate the rigid bodies.
-	b2World world(gravity);
-  world.SetContinuousPhysics(false);
-
-  InitWorld(&world);
-
-	// Prepare for simulation. Typically we use a time step of 1/60 of a
-	// second (60Hz) and 10 iterations. This provides a high quality simulation
-	// in most game scenarios.
+	b2Vec2 gravity;
 	float timeStep = 1.0f / 60.0f;
 	int32 velocityIterations = 8;
 	int32 positionIterations = 3;
+	int32 simulationSteps = 500;
+	std::string name = "";
+};
 
-  using std::chrono::high_resolution_clock;
-	auto start = high_resolution_clock::now();
-	
-	// This is our little game loop.
-	for (int32 i = 0; i < 400; ++i) {
-		// Instruct the world to perform a single step of simulation.
-		// It is generally best to keep the time step and iterations fixed.
-		world.Step(timeStep, velocityIterations, positionIterations);
-	}
-	
-	auto finish = high_resolution_clock::now();
-	int64 time = (finish - start).count();
+int main() {
+  std::vector<b2Benchmark*> benchmarks;
+  
+  class : public b2Benchmark {
+    virtual void InitBenchmark() override {
+      name = "Falling squares";
+      simulationSteps = 1300;
+    }
+    
+    virtual void InitWorld(b2World* world) override {
+      {
+			  b2BodyDef bd;
+			  b2Body* ground = world->CreateBody(&bd);
+
+			  b2PolygonShape shape;
+			  b2Vec2 position(0, -10);
+			  shape.SetAsBox(100, 3, position, 0.0f);
+			  ground->CreateFixture(&shape, 0.0f);
+		  }
+
+		  for (int32 i = 0; i < 15; ++i) {
+		    float a = 0.5f + i / 15.0f * 2.5f;
+        b2PolygonShape shape;
+		    shape.SetAsBox(a, a);
+			  
+			  for (int32 j = i; j < 300; ++j) {
+				  b2BodyDef bd;
+				  bd.type = b2_dynamicBody;
+				  bd.position.x = i * 7 - 30;
+				  bd.position.y = 300 * 2 * a - j * 2 * a;
+				  
+				  b2Body* body = world->CreateBody(&bd);
+			    body->CreateFixture(&shape, 5.0f);
+			  }
+		  }
+    }
+  } b1;
+  benchmarks.push_back(&b1);
+  
+  class : public b2Benchmark {
+    virtual void InitBenchmark() override {
+      name = "Falling circles";
+      simulationSteps = 1300;
+    }
+    
+    virtual void InitWorld(b2World* world) override {
+      {
+			  b2BodyDef bd;
+			  b2Body* ground = world->CreateBody(&bd);
+
+			  b2PolygonShape shape;
+			  b2Vec2 position(0, -10);
+			  shape.SetAsBox(100, 3, position, 0.0f);
+			  ground->CreateFixture(&shape, 0.0f);
+		  }
+
+		  for (int32 i = 0; i < 15; ++i) {
+		    float a = 0.5f + i / 15.0f * 2.5f;
+        b2CircleShape shape;
+		    shape.m_radius = a / 2;
+			  
+			  for (int32 j = i; j < 300; ++j) {
+				  b2BodyDef bd;
+				  bd.type = b2_dynamicBody;
+				  bd.position.x = i * 7 + j * 0.25f - 100;
+				  bd.position.y = 300 * 2 * a - j * 2 * a;
+				  
+				  b2Body* body = world->CreateBody(&bd);
+			    body->CreateFixture(&shape, 5.0f);
+			  }
+		  }
+    }
+  } b2;
+  benchmarks.push_back(&b2);
+
+  class : public b2Benchmark {
+    virtual void InitBenchmark() override {
+      name = "Tumbler";
+      simulationSteps = 1500;
+    }
+
+    virtual void InitWorld(b2World* world) override {
+      b2Body* ground = NULL;
+		  
+		  {
+			  b2BodyDef bd;
+			  ground = world->CreateBody(&bd);
+		  }
+
+		  {
+			  b2BodyDef bd;
+			  bd.type = b2_dynamicBody;
+			  bd.allowSleep = false;
+			  bd.position.Set(0.0f, 10.0f);
+			  b2Body* body = world->CreateBody(&bd);
+
+			  b2PolygonShape shape;
+			  shape.SetAsBox(0.5f, 10.0f, b2Vec2( 10.0f, 0.0f), 0.0);
+			  body->CreateFixture(&shape, 5.0f);
+			  shape.SetAsBox(0.5f, 10.0f, b2Vec2(-10.0f, 0.0f), 0.0);
+			  body->CreateFixture(&shape, 5.0f);
+			  shape.SetAsBox(10.0f, 0.5f, b2Vec2(0.0f, 10.0f), 0.0);
+			  body->CreateFixture(&shape, 5.0f);
+			  shape.SetAsBox(10.0f, 0.5f, b2Vec2(0.0f, -10.0f), 0.0);
+			  body->CreateFixture(&shape, 5.0f);
+
+			  b2RevoluteJointDef jd;
+			  jd.bodyA = ground;
+			  jd.bodyB = body;
+			  jd.localAnchorA.Set(0.0f, 10.0f);
+			  jd.localAnchorB.Set(0.0f, 0.0f);
+			  jd.referenceAngle = 0.0f;
+			  jd.motorSpeed = 0.05f * b2_pi;
+			  jd.maxMotorTorque = 1e8f;
+			  jd.enableMotor = true;
+			  world->CreateJoint(&jd);
+		  }
+    }
+    
+    int32 m_count = 0;
+    
+    virtual void StepWorld(b2World* world) override {
+      const int32 e_count = 1000;
+      
+      if (m_count < e_count) {
+			  b2BodyDef bd;
+			  bd.type = b2_dynamicBody;
+			  bd.position.Set(0.0f, 10.0f);
+			  b2Body* body = world->CreateBody(&bd);
+
+			  b2PolygonShape shape;
+			  shape.SetAsBox(0.125f, 0.125f);
+			  body->CreateFixture(&shape, 1.0f);
+
+			  ++m_count;
+		  }
+    }
+  } b3;
+  benchmarks.push_back(&b3);
+
+  class : public b2Benchmark {
+    virtual void InitBenchmark() override {
+      name = "Add pair";
+      simulationSteps = 1000;
+      gravity = b2Vec2(0.0f, 0.0f);
+    }
+    
+    virtual void InitWorld(b2World* world) override {
+		  {
+			  b2CircleShape shape;
+			  shape.m_p.SetZero();
+			  shape.m_radius = 0.1f;
+
+			  float minX = -9.0f;
+			  float maxX = 9.0f;
+			  float minY = 4.0f;
+			  float maxY = 6.0f;
+			  
+			  for (int32 i = 0; i < 2000; ++i)
+			  {
+				  b2BodyDef bd;
+				  bd.type = b2_dynamicBody;
+				  bd.position = b2Vec2(minX + (maxX - minX) * i / 2000.0f, minY + (maxY - minY) * (i % 32) / 32.0f);
+				  b2Body* body = world->CreateBody(&bd);
+				  body->CreateFixture(&shape, 0.01f);
+			  }
+		  }
+		  
+		  {
+			  b2PolygonShape shape;
+			  shape.SetAsBox(1.5f, 1.5f);
+			  b2BodyDef bd;
+			  bd.type = b2_dynamicBody;
+			  bd.position.Set(-40.0f,5.0f);
+			  b2Body* body = world->CreateBody(&bd);
+			  body->CreateFixture(&shape, 1.0f);
+			  body->SetLinearVelocity(b2Vec2(150.0f, 0.0f));
+		  }
+    }
+  } b4;
+  benchmarks.push_back(&b4);
+  
+  for (auto benchmark : benchmarks) {
+    benchmark->InitBenchmark();
+    int32 iterations = 1;
+    int64 totalTime = 0;
+    
+    for (int32 i = 0; i < iterations; ++i) {
+	    // Construct a world object, which will hold and simulate the rigid bodies.
+	    b2World world(benchmark->gravity);
+	    world.SetContinuousPhysics(false);
+
+      benchmark->InitWorld(&world);
+
+      using std::chrono::high_resolution_clock;
+	    auto start = high_resolution_clock::now();
+	    
+	    // This is our little game loop.
+	    for (int32 i = 0; i < benchmark->simulationSteps; ++i) {
+		    // Instruct the world to perform a single step of simulation.
+		    // It is generally best to keep the time step and iterations fixed.
+		    world.Step(benchmark->timeStep, benchmark->velocityIterations, benchmark->positionIterations);
+		    benchmark->StepWorld(&world);
+	    }
+	    
+	    auto finish = high_resolution_clock::now();
+	    int64 time = (finish - start).count();
+	    totalTime += time;
+    }
+    
+    std::cout.precision(std::numeric_limits<float>::digits10);
+    std::cout << std::left << std::setw(20) << benchmark->name << ": "
+              << std::right << std::setw(15) << (totalTime / 1000000.0 / iterations) << " ms" << std::endl;
+  }
+
+/*
 	int32 mem = -1;
 	
 #if __linux__
@@ -133,6 +282,7 @@ int main() {
 #endif
   
   std::cout << "time: " << time << ", mem: " << mem << std::endl;
+*/
 
 	return 0;
 }
